6cc0a8ed06a97c7c729c02272fc4e27227004423
kenny <kenny.kwyiu@gmail.com>
2023-11-29 18:28:20 +0800

    29 Nov 2023 Day6OperatorsStringsAndLoops with Test test is not work :(

---
+++ src/main/java/kenny/Day6OperatorsStringsAndLoops.java
@@ -0,0 +1,30 @@
+package kenny;
+
+import java.util.Scanner;
+
+public class Day6OperatorsStringsAndLoops {
+
+    public static void printEvenAndOddCharacters() {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < input; i++) {
+            String word1 = scanner.nextLine();
+            StringBuilder evenWord1 = new StringBuilder();
+            StringBuilder oddWord1 = new StringBuilder();
+
+            for (int j = 0; j < word1.length(); j++) {
+                if (j % 2 == 0) {
+                    evenWord1.append(word1.charAt(j));
+                } else {
+                    oddWord1.append(word1.charAt(j));
+                }
+            }
+            System.out.println(evenWord1.toString() + " " + oddWord1.toString());
+        }
+        scanner.close();
+
+    }
+
+}
---
+++ src/test/java/kenny/Day6OperatorsStringsAndLoopsTest.java
@@ -0,0 +1,41 @@
+package kenny;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+public class Day6OperatorsStringsAndLoopsTest {
+
+    @Test
+    public void testPrintEvenAndOddCharacters() {
+        // Prepare test input
+        String input = "3\nHacker\nRank\nDay";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        // Redirect System.out to capture the console output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalOut = System.out;
+        System.setOut(printStream);
+
+        // Execute the code under test
+        Day6OperatorsStringsAndLoops.printEvenAndOddCharacters();
+
+        // Restore original System.out
+        System.out.flush();
+        System.setOut(originalOut);
+
+        // Get the captured output as a string
+        String consoleOutput = outputStream.toString().trim();
+
+        // Define the expected output
+        String expectedOutput = "Hce akr\nRn ak\nDy a";
+
+        // Assert the expected output matches the console output
+        Assert.assertEquals(expectedOutput, consoleOutput);
+    }
+}
