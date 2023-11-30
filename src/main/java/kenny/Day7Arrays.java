04b66be4587f732694995335c2e12d4d95da25b6
kenny <kenny.kwyiu@gmail.com>
2023-11-30 14:07:29 +0800

    30 Nov 2023 Day7Arrays - added reverseAndPrintOutArray method test

--- src/main/java/kenny/Day7Arrays.java
+++ src/main/java/kenny/Day7Arrays.java
@@ -66,4 +66,6 @@ public class Day7Arrays {
         bufferedReader.close();
     }
 
+
+
 }
--- src/test/java/kenny/Day7ArraysTest.java
+++ src/test/java/kenny/Day7ArraysTest.java
@@ -41,4 +41,33 @@ public class Day7ArraysTest {
         // Assert the expected output matches the console output
         Assert.assertEquals(expectedOutput, consoleOutput);
     }
+
+    @Test
+    public void testReverseAndPrintOutArrayMethod() {
+        // Prepare test input
+        int n = 5;
+        List<Integer> arr = Arrays.asList(1, 2, 3, 4, 5);
+
+        // Redirect System.out to capture the console output
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalOut = System.out;
+        System.setOut(printStream);
+
+        // Execute the code under test
+        Day7Arrays.reverseAndPrintOutArray(n, arr);
+
+        // Restore original System.out
+        System.out.flush();
+        System.setOut(originalOut);
+
+        // Get the captured output as a string
+        String consoleOutput = outputStream.toString().trim();
+
+        // Define the expected output
+        String expectedOutput = "5 4 3 2 1";
+
+        // Assert the expected output matches the console output
+        Assert.assertEquals(expectedOutput, consoleOutput);
+    }
 }
