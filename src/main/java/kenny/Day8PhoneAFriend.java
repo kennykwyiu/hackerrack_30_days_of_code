c72448e18878e36352e1aad19fa0e35797c002bb
kenny <kenny.kwyiu@gmail.com>
2023-12-01 11:35:05 +0800

    1 Dec 2023 Day8PhoneAFriend - You are given a phone book that consists of people's names and their phone numbers. After that, you will be given some person's name as a query. For each query, print the person's name and phone number if it is present in the phone book; otherwise, print "Not found".

---
+++ src/main/java/kenny/Day8PhoneAFriend.java
@@ -0,0 +1,71 @@
+package kenny;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * Question
+ * You are given a phone book that consists of people's names and their phone numbers.
+ * After that, you will be given some person's name as a query.
+ * For each query, print the person's name and phone number if it is present in the phone book;
+ * otherwise, print "Not found".
+ *
+ * Input Format
+ * The first line will have an integer, n, representing the number of entries in the phone book.
+ * Each entry consists of two lines: a name and the corresponding phone number, separated by a newline.
+ *
+ * After the n lines of phone book entries,
+ * there are an unknown number of lines of queries. Each line (query) contains a name to look up,
+ * and you must continue reading lines until there is no more input.
+ *
+ * Output Format
+ * For each query, print the person's name and phone number if it is present in the phone book;
+ * otherwise, print "Not found".
+ *
+ * Sample Input
+ *
+ * 3
+ * Sam
+ * 99912222
+ * Tom
+ * 11122222
+ * Harry
+ * 12299933
+ * Sam
+ * Ed
+ * Tom
+ *
+ * Sample Output
+ *
+ * Sam=99912222
+ * Not found
+ * Tom=11122222
+ *
+ */
+public class Day8PhoneAFriend {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        Map<String, Integer> phoneBook = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            phoneBook.put(name, phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if (phoneBook.containsKey(s)) {
+                System.out.println(s + "=" + phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
---
+++ src/test/java/kenny/Day8PhoneAFriendTest.java
@@ -0,0 +1,35 @@
+package kenny;
+
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import static org.junit.Assert.*;
+
+public class Day8PhoneAFriendTest {
+
+    @Test
+    public void testPhoneBook() {
+        String input = "3\nJohn 123\nJane 456\nAlice 789\nJohn\nJane\nBob\n";
+        String expectedOutput = "John=123\r\nJane=456\r\nNot found\r\n";
+
+        InputStream sysInBackup = System.in;
+        PrintStream sysOutBackup = System.out;
+
+        try {
+            ByteArrayInputStream inContent = new ByteArrayInputStream(input.getBytes());
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+            System.setIn(inContent);
+            System.setOut(new PrintStream(outContent));
+
+            Day8PhoneAFriend.main(null);
+
+            assertEquals(expectedOutput, outContent.toString());
+        } finally {
+            System.setIn(sysInBackup);
+            System.setOut(sysOutBackup);
+        }
+    }
+}
