376faf4138f74a73116fff1c22597042377c5cb7
kenny <kenny.kwyiu@gmail.com>
2023-12-16 10:46:14 +0800

    16 Dec 2023 Day22BinarySearchTrees

---
+++ src/main/java/kenny/thirtydays/Day22BinarySearchTrees.java
@@ -0,0 +1,53 @@
+package kenny.thirtydays;
+
+import java.util.Scanner;
+
+public class Day22BinarySearchTrees {
+    public static int getHeight(TreeNode root) {
+        if (root == null) {
+            return -1;
+        }
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public static TreeNode insert(TreeNode root, int data) {
+        if (root == null) {
+            return new TreeNode(data);
+        } else {
+            TreeNode cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        TreeNode root = null;
+        while (T-- > 0) {
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+        int height = getHeight(root);
+        System.out.println(height);
+        sc.close();
+    }
+}
+
+class TreeNode {
+    TreeNode left, right;
+    int data;
+
+    TreeNode(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
