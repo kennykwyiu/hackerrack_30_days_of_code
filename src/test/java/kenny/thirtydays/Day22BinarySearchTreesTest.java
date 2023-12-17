d6c7da3c854d473c18450f77e8a0db3aa01843d0
kenny <kenny.kwyiu@gmail.com>
2023-12-17 09:59:27 +0800

    17 Dec 2023 Day23 - testLevelOrder() with test

--- src/test/java/kenny/thirtydays/Day22BinarySearchTreesTest.java
+++ src/test/java/kenny/thirtydays/Day22BinarySearchTreesTest.java
@@ -1,9 +1,49 @@
 package kenny.thirtydays;
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
 public class Day22BinarySearchTreesTest {
+
+    @Test
+    public void testLevelOrder() {
+        // Create a binary search tree with the following values:
+        //       4
+        //      / \
+        //     2   6
+        //    / \   \
+        //   1   3   7
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+
+        // Redirect the standard output to capture the printed level order traversal
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalPrintStream = System.out;
+        System.setOut(printStream);
+
+        // Call the levelOrder method
+        Day22AndDay23BinarySearchTrees.levelOrder(root);
+
+        // Restore the original standard output
+        System.out.flush();
+        System.setOut(originalPrintStream);
+
+        // Get the printed level order traversal
+        String printedTraversal = outputStream.toString().trim();
+
+        // Validate the printed level order traversal
+        String expectedTraversal = "4 2 6 1 3 7";
+        assertEquals(expectedTraversal, printedTraversal);
+    }
 
     @Test
     public void testGetHeight() {
@@ -21,7 +61,7 @@ public class Day22BinarySearchTreesTest {
         root.right.right = new TreeNode(7);
 
         int expectedHeight = 2;  // The height of the tree is 2
-        int actualHeight = Day22BinarySearchTrees.getHeight(root);
+        int actualHeight = Day22AndDay23BinarySearchTrees.getHeight(root);
         assertEquals(expectedHeight, actualHeight);
     }
 
@@ -30,13 +70,13 @@ public class Day22BinarySearchTreesTest {
         TreeNode root = null;
 
         // Insert values 5, 3, 7, 2, 4, 6, 8 into the binary search tree
-        root = Day22BinarySearchTrees.insert(root, 5);
-        root = Day22BinarySearchTrees.insert(root, 3);
-        root = Day22BinarySearchTrees.insert(root, 7);
-        root = Day22BinarySearchTrees.insert(root, 2);
-        root = Day22BinarySearchTrees.insert(root, 4);
-        root = Day22BinarySearchTrees.insert(root, 6);
-        root = Day22BinarySearchTrees.insert(root, 8);
+        root = Day22AndDay23BinarySearchTrees.insert(root, 5);
+        root = Day22AndDay23BinarySearchTrees.insert(root, 3);
+        root = Day22AndDay23BinarySearchTrees.insert(root, 7);
+        root = Day22AndDay23BinarySearchTrees.insert(root, 2);
+        root = Day22AndDay23BinarySearchTrees.insert(root, 4);
+        root = Day22AndDay23BinarySearchTrees.insert(root, 6);
+        root = Day22AndDay23BinarySearchTrees.insert(root, 8);
 
         // Validate the values and structure of the resulting tree
         assertEquals(5, root.data);
